4a2e1976cce72ff84d14afee2f3600f40e61fb8d
nwon24 <nathanw2047@gmail.com>
2023-07-22 03:06:01 +1000

    Slash comment

--- boot/asm.s
+++ boot/asm.s
@@ -1,11 +1,11 @@
-# Bootloader
-# Loads /kernel from the filesystem
-# Begins in 16-bit real mode
-# Linked at 0x7e00
+/ Bootloader
+/ Loads /kernel from the filesystem
+/ Begins in 16-bit real mode
+/ Linked at 0x7e00
 
-# MEMORY MAP
-# 0x1000 - 0x5000 - page tables
-# 0x5000 - memory map
+/ MEMORY MAP
+/ 0x1000 - 0x5000 - page tables
+/ 0x5000 - memory map
 
 COM1 = 0x3f8
 
@@ -13,47 +13,47 @@ COM1 = 0x3f8
 .text
 .globl _start
 _start:
-	# Try to enable A20
-	mov	$0x2403,%ax	# A20 supported?
+	/ Try to enable A20
+	mov	$0x2403,%ax	/ A20 supported?
 	int	$0x15
-	jb	a20err		# No supported
+	jb	a20err		/ No supported
 
-	mov	$0x2402,%ax	# A20 enabled?
+	mov	$0x2402,%ax	/ A20 enabled?
 	int	$0x15
-	jb	a20err		# Error
-	cmp	$1,%al		# A20 enabled?
-	je	1f		# Already enabled
-	mov	$0x2401,%ax	# Enable A20
+	jb	a20err		/ Error
+	cmp	$1,%al		/ A20 enabled?
+	je	1f		/ Already enabled
+	mov	$0x2401,%ax	/ Enable A20
 	int	$0x15
-	jb	a20err		# Failed
+	jb	a20err		/ Failed
 1:
-	# Print start message
+	/ Print start message
 	mov	$bootmsg,%si
 	call	print16
 
-	# Go to protected mode
-	cli			# No interrupts
-	lgdt	bootgdtr	# Load gdt
+	/ Go to protected mode
+	cli			/ No interrupts
+	lgdt	bootgdtr	/ Load gdt
 	mov	%cr0,%eax
-	or	$1,%al		# PM bit on
+	or	$1,%al		/ PM bit on
 	mov	%eax,%cr0
-	ljmp	$0x8,$pmmode	# Jump to protected mode
+	ljmp	$0x8,$pmmode	/ Jump to protected mode
 	jmp	.
 
-	# Print message while still in real mode
-	# Null terminated string in si
+	/ Print message while still in real mode
+	/ Null terminated string in si
 print16:
-	mov	$0,%dx		# First serial port
-1:	lodsb			# Get byte
-	test	%al,%al		# Done?
-	jz	1f		# Yes, exit
-	mov	$1,%ah		# No, print it
+	mov	$0,%dx		/ First serial port
+1:	lodsb			/ Get byte
+	test	%al,%al		/ Done?
+	jz	1f		/ Yes, exit
+	mov	$1,%ah		/ No, print it
 	int	$0x14
-	jmp	1b		# loop
+	jmp	1b		/ loop
 1:	ret
 
-	# Print message and loop forever while in real mode
-	# Message in si
+	/ Print message and loop forever while in real mode
+	/ Message in si
 err16:
 	call	print16
 	jmp	.
@@ -65,37 +65,37 @@ a20err:
 a20msg:
 	.asciz	"?a20"
 bootmsg:
-	.asciz	"boot\n"
+	.asciz	"boot\r\n"
 
-	# GDT
-	# Set up for protected mode
+	/ GDT
+	/ Set up for protected mode
 bootgdt:
-	# NULL
+	/ NULL
 	.long	0,0
 
-	# Code: r/w, 0xffff, base 0, gran 1
-	.word	0xffff	# limit 0..15
-	.word	0	# base
-	.byte	0	# base
-	.byte	0x9a	# type r/x, code, present 
-	.byte	0xcf	# limit, 32-bit segment, granularity 1
-	.byte	0	# base
+	/ Code: r/w, 0xffff, base 0, gran 1
+	.word	0xffff	/ limit 0..15
+	.word	0	/ base
+	.byte	0	/ base
+	.byte	0x9a	/ type r/x, code, present 
+	.byte	0xcf	/ limit, 32-bit segment, granularity 1
+	.byte	0	/ base
 
 	.word	0xffff
 	.word	0
 	.byte	0
-	.byte	0x92	# type r/w data, present
+	.byte	0x92	/ type r/w data, present
 	.byte	0xcf
 	.byte	0
 bootgdtr:
 	.word	bootgdtr-bootgdt
 	.long	bootgdt
 
-# START OF 32BIT
+/ START OF 32BIT
 .code32
 pmmode:
-	cli			# No interrupts
-	mov	$0x10,%ax	# Set segment descriptors
+	cli			/ No interrupts
+	mov	$0x10,%ax	/ Set segment descriptors
 	mov	%ax,%ds
 	mov	%ax,%es
 	mov	%ax,%fs
@@ -103,65 +103,65 @@ pmmode:
 	mov	%ax,%ss
 	mov	$pmstack,%esp
 
-	# Set up serial port for output
-	# Disable all interrupts
-	# Write 0 to port +1
+	/ Set up serial port for output
+	/ Disable all interrupts
+	/ Write 0 to port +1
 	mov	$0,%al
 	mov	$COM1+1,%dx
 	out	%al,%dx
-	# Set baud rate
-	# Begin by setting DLAB in port +3
+	/ Set baud rate
+	/ Begin by setting DLAB in port +3
 	mov	$0x80,%al
 	mov	$COM1+3,%dx
 	out	%al,%dx
-	# Baud rate 115200 (divisor 1)
+	/ Baud rate 115200 (divisor 1)
 	mov	$1,%al
 	mov	$COM1,%dx
 	out	%al,%dx
 	mov	$0,%al
 	inc	%dx
 	out	%al,%dx
-	# Clear msb of port +3
-	# and set 8 bit, no parity, one stop bit
+	/ Clear msb of port +3
+	/ and set 8 bit, no parity, one stop bit
 	mov	$0x3,%al
 	mov	$COM1+3,%dx
 	out	%al,%dx
-	# magic
+	/ magic
 	mov	$0xc7,%al
 	mov	$COM1+2,%dx
 	out	%al,%dx
 
-	# Done. Now call main to load the kernel.
+	/ Done. Now call main to load the kernel.
 	call	main
-	# Unreachable
+	/ Unreachable
 die:	jmp	.
 
 .globl putc
-	# Send a character to the serial port
+	/ Send a character to the serial port
 putc:
-	mov	$COM1+5,%dx	# Line status register
-	inb	%dx,%al		# Get status
-	and	$0x20,%al	# Can data be send?
-	jz	putc		# No, loop
-	mov	4(%esp),%eax	# Get char
+	mov	$COM1+5,%dx	/ Line status register
+	inb	%dx,%al		/ Get status
+	and	$0x20,%al	/ Can data be send?
+	jz	putc		/ No, loop
+	mov	4(%esp),%eax	/ Get char
 	mov	$COM1,%dx
-	out	%al,%dx		# Send it
+	out	%al,%dx		/ Send it
 	ret
 .globl puts
-	# Write a string to the serial port
+	/ Write a string to the serial port
 puts:
 	push	%ebp
 	mov	%esp,%ebp
 	push	%esi
-	mov	8(%ebp),%esi	# Get string
-1:	xor	%eax,%eax	# Clear %eax
-	lodsb			# Get a byte
-	test	%al,%al		# End of string?
-	jz	1f		# Yes, exit loop
-	push	%eax		# Call 'putc' to print it
+	mov	8(%ebp),%esi	/ Get string
+1:	xor	%eax,%eax	/ Clear %eax
+	lodsb			/ Get a byte
+	test	%al,%al		/ End of string?
+	jz	1f		/ Yes, exit loop
+	push	%eax		/ Call 'putc' to print it
 	call	putc
 	add	$4,%esp	
-	jmp	1b		# Loop
+	jmp	1b		/ Loop
 1:	pop	%esi
 	mov	%ebp,%esp
 	pop	%ebp
