1df6c98a04b3171bf30d0d56bf0f7dcfd9399a4b
nwon24 <nathanw2047@gmail.com>
2023-07-21 21:42:15 +1000

    Slash comments

--- boot0.s
+++ boot0.s
@@ -1,4 +1,4 @@
-# 16-bit boot block (MBR)
+/ 16-bit boot block (MBR)
 .=0
 LADDR	= 0x7c00
 LSEG	= 0x7c0
@@ -12,7 +12,7 @@ LBAOFF  = 8
 .globl _start
 _start:
 	ljmp	$0,$1f
-1:	cli		# No interrupts
+1:	cli		/ No interrupts
 	mov	$0,%ax
 	mov	%ax,%ss
 	mov	$LADDR,%sp
@@ -21,81 +21,81 @@ _start:
 	mov	%ax,%fs
 	mov	%ax,%gs
 
-	# Copy to RADDR
-	mov	$256,%cx	# Copy 256 words
-	mov	$RADDR,%di	# destination
-	mov	$LADDR,%si	# Source
-	cld			# Go forward
+	/ Copy to RADDR
+	mov	$256,%cx	/ Copy 256 words
+	mov	$RADDR,%di	/ destination
+	mov	$LADDR,%si	/ Source
+	cld			/ Go forward
 	rep
-	movsw			# Copy
-	ljmp	$0,$RADDR+1f-LADDR	# Jump to relocation
+	movsw			/ Copy
+	ljmp	$0,$RADDR+1f-LADDR	/ Jump to relocation
 
-	# From now on, addresses must be relative to RADDR
+	/ From now on, addresses must be relative to RADDR
 1:	sti
-	cmp	$0x80,%dl		# Floppy disk?
-	jl	err			# Don't care for them
-	# Save disk
+	cmp	$0x80,%dl		/ Floppy disk?
+	jl	err			/ Don't care for them
+	/ Save disk
 	movb	%dl,RADDR+drv-LADDR
-	# Set es:di to part table
+	/ Set es:di to part table
 	mov	$RADDR+PARTOFF,%di
 
-	# Print prompt :
-	mov	$0x1,%ah		# Send output to serial port
-	mov	$0,%dx			# Serial port 1
-	mov	$':',%al		# Character
+	/ Print prompt :
+	mov	$0x1,%ah		/ Send output to serial port
+	mov	$0,%dx			/ Serial port 1
+	mov	$':',%al		/ Character
 	int	$0x14
-	test	$128,%ah		# Error?
-	jnz	err			# Wouldn't help, but do it anyway
+	test	$128,%ah		/ Error?
+	jnz	err			/ Wouldn't help, but do it anyway
 
-	# Get a number from the user 1-4
+	/ Get a number from the user 1-4
 	mov	$0,%ah
 	int	$0x16
-	cmp	$'1',%al		# < '1'?
-	jl	err			# Yes, error
-	cmp	$'4',%al		# > '4'?'
-	jg	err			# Yes, error
-	movb	$0,%ah			# Clear upper byte
-	push	%ax			# Save it
-	sub	$'0',%al		# Get partition number
-	mov	%al,RADDR+part-LADDR		# save it
-	pop	%ax			# Echo it back
+	cmp	$'1',%al		/ < '1'?
+	jl	err			/ Yes, error
+	cmp	$'4',%al		/ > '4'?'
+	jg	err			/ Yes, error
+	movb	$0,%ah			/ Clear upper byte
+	push	%ax			/ Save it
+	sub	$'0',%al		/ Get partition number
+	mov	%al,RADDR+part-LADDR		/ save it
+	pop	%ax			/ Echo it back
 	mov	$0x1,%ah
-	mov	$0,%dx			# First serial port
-	int	$0x14			# Send it
+	mov	$0,%dx			/ First serial port
+	int	$0x14			/ Send it
 
-	# Get partition table entry
+	/ Get partition table entry
 	xor	%ax,%ax
 	mov	RADDR+part-LADDR,%al
 	dec	%al
-	shl	$4,%al			# Each entry is 16 bytes
+	shl	$4,%al			/ Each entry is 16 bytes
 	add	%ax,%di
-	mov	(%di),%al		# Get first byte
-	test	$0x80,%al		# Bootable?
-	jz	err			# No, error
+	mov	(%di),%al		/ Get first byte
+	test	$0x80,%al		/ Bootable?
+	jz	err			/ No, error
 	
-	# Now read the VBR
-	mov	LBAOFF(%di), %ax	# Lower 16-bits
-	mov	LBAOFF+2(%di), %bx	# Upper 16-bits
+	/ Now read the VBR
+	mov	LBAOFF(%di), %ax	/ Lower 16-bits
+	mov	LBAOFF+2(%di), %bx	/ Upper 16-bits
 	mov	$RADDR+pket-LADDR,%si
-	mov	%ax,8(%si)		# Store in packet
-	mov	%bx,10(%si)		# ""
+	mov	%ax,8(%si)		/ Store in packet
+	mov	%bx,10(%si)		/ ""
 	movw	$0,12(%si)
 	
-	mov	$0x42,%ah		# Extended read
-	mov	RADDR+drv-LADDR,%dl			# Drive number
+	mov	$0x42,%ah		/ Extended read
+	mov	RADDR+drv-LADDR,%dl			/ Drive number
 	int	$0x13
-	jc	err			# Carry set on error
-	test	%ah,%ah			# ah should also be 0
+	jc	err			/ Carry set on error
+	test	%ah,%ah			/ ah should also be 0
 	jnz	err
 
-	# Give partition table entry and disk to VBR
+	/ Give partition table entry and disk to VBR
 	mov	%di,%si
 	mov	RADDR+drv-LADDR,%dl
-	# Now jump to VBR
+	/ Now jump to VBR
 	ljmp	$0,$LADDR
 1:	jmp	.
 
-	# err: print '?' and loop forever
+	/ err: print '?' and loop forever
 err:	mov	$0x1,%ah
 	mov	$0,%dx
 	mov	$'?',%al
