c28c5af3c82e0aea9a358d0f6fea68243cc97ef7
nwon24 <nathanw2047@gmail.com>
2023-07-26 08:28:25 +1000

    Get command and store in buffer

--- boot/main.c
+++ boot/main.c
@@ -1,22 +1,37 @@
 #include <stdint.h>
 
+#define LBSIZ 20
+
 void putc(int c);
 void puts(char *s);
 int getc(void);
 
 void iderd(void *buf, int lba, int count);
 
+char lnbuf[LBSIZ];
+
 void
 main(void)
 {
+	char *p;
+	int cr;
+
+	p = lnbuf;
+	cr = 0;
 	puts("Hello, world!\n");
 	while (1) {
 		int c;
 
 		c = getc();
-		if (c == '\r')
+		if (c == '\r') {
 			putc('\n');
+			++cr;
+		}
 		putc(c);
+		if (cr || p >= lnbuf+LBSIZ)
+			break;
+		*p++ = c;
 	}
+	puts("command\r\n");
 	while (1);
 }
